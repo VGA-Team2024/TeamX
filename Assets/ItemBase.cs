b8251df63f754aa9003a6f6159d358624a94b1c3
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2024-02-08 23:21:28 +0900

    [fix]ItemBaseをGearManagerのGearInfoリストに自分を追加するように修正しました。 Buttonにアタッチして各パラメータをいれれば、場にGearManagerがある場合スコアを使って買うことができます。 ※ItemBaseは継承して使う必要があります。

--- Assets/ItemBase.cs
+++ Assets/ItemBase.cs
@@ -1,21 +1,44 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 public abstract class ItemBase : MonoBehaviour
 {
+    Text _text;
+    void Start()
+    {
+        _text = GetComponentInChildren<Text>();
+        _text.text =  $"{itemStruct.ItemName} {(int)itemStruct.ItemPrice}" ;
+        GearInfo gearInfo = new();
+        gearInfo.GearName = itemStruct.ItemName;
+        gearInfo.GearSps = itemStruct.ItemSps;
+        gearInfo.GearValue = 0;
+        GearManager.Instance.Gears.Add(gearInfo);
+    }
     const double _buyMag = 1.15;
     [Serializable]
     public struct ItemStruct
     {
         /// <summary>アイテムの名前。例：グランマ </summary>
         public string ItemName;
-        /// <summary>アイテムの最初の値段。例：100</summary>
+        /// <summary>アイテムの値段。例：100</summary>
         public double ItemPrice;
+        /// <summary>アイテムのsps。例：1</summary>
+        public float ItemSps;
     }
 
-    [SerializeField]protected ItemStruct itemStruct = new();
+    [SerializeField] protected ItemStruct itemStruct = new();
     public void MulIntemBuyMag() => itemStruct.ItemPrice *= _buyMag;
+
+    public bool IsBuy() => ScoreManager.Instance.Score > (int)itemStruct.ItemPrice;
+    
     public virtual void BoughItem()
     {
-        MulIntemBuyMag();
+        if (IsBuy())
+        {
+            ScoreManager.Instance.SubScore((int)itemStruct.ItemPrice);
+            MulIntemBuyMag();
+            GearManager.Instance.AddGear(itemStruct.ItemName);
+            _text.text = $"{itemStruct.ItemName} {(int)itemStruct.ItemPrice}";
+        }
     }
 }
