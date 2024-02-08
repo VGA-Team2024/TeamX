6081a4e6f4200ac93f41a19fb361f2fa9a635e94
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2024-02-08 22:17:09 +0900

    [Fix]ItemBaseの変更

--- Assets/ItemBase.cs
+++ Assets/ItemBase.cs
@@ -1,18 +1,21 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public abstract class ItemBase : MonoBehaviour
 {
-    /// <summary>アイテムの名前。例：グランマ</summary>
-    public abstract string ItemName();
-    /// <summary>アイテムの最初の値段。例：100</summary>
-    public abstract int ItemInitialPrice();
-    /// <summary>買うごとに値段に乗算する倍率(定数)</summary>
-    protected const float buyMag = 1.15f;
-    /// <summary>
-    /// このアイテムが買われたときに行う処理を書く
-    /// </summary>
-    public abstract void BoughtItem();
+    const double _buyMag = 1.15;
+    [Serializable]
+    public struct ItemStruct
+    {
+        /// <summary>アイテムの名前。例：グランマ </summary>
+        public string ItemName;
+        /// <summary>アイテムの最初の値段。例：100</summary>
+        public double ItemPrice;
+    }
+
+    [SerializeField]protected ItemStruct itemStruct = new();
+    public void MulIntemBuyMag() => itemStruct.ItemPrice *= _buyMag;
+    public virtual void BoughItem()
+    {
+        MulIntemBuyMag();
+    }
 }
