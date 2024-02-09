8044abc55dbfcd7be3a23cca3374ae4f42bc996d
kuromaaaaa <kuromaxa@gmail.com>
2024-02-09 23:44:57 +0900

    [add,fix]UpGradeの追加,TextMeshPro用にスクリプトを一部変更,ゲーム開始時にScoreと施設リストが変更されていない部分を修正

---
+++ Assets/UpGrade.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpGrade : MonoBehaviour
+{
+    [SerializeField] int _price;
+    [SerializeField] string[] _upGradeGears;
+    [SerializeField] string _UpGradeNameText;
+
+    private void Start()
+    {
+        GetComponentInChildren<TMP_Text>().text = $"{_UpGradeNameText} {_price}";
+    }
+
+    public void BoughUpGrade()
+    {
+        if(ScoreManager.Instance.Score > _price)
+        {
+            ScoreManager.Instance.SubScore(_price);
+            foreach(string s in _upGradeGears)
+            {
+                GameManager.Instance.UpGrade(s);
+            }
+            OverRideBough();
+            Destroy(gameObject);
+        }
+    }
+
+    protected virtual void OverRideBough()
+    {
+
+    }
+}
