010fed12e63c68234a5c75f54ec2a7770581d990
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2024-02-09 16:39:12 +0900

    [add]Pocketスクリプトの追加。 Clickedメソッドを実行するとScoreManagerにスコアが追加されます。

---
+++ Assets/Pocket.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pocket 
+{
+    [SerializeField] int _clickedAddScore = 1;
+
+    public void Clicked()
+    {
+        ScoreManager.Instance.AddScore(_clickedAddScore);
+    }
+}
