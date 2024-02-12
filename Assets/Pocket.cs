9adac7c5ca346499ec5fcb9c6ecb6b54e372b8cc
Kitamura Haruki <90311063+629Kitamura@users.noreply.github.com>
2024-02-12 14:29:25 +0900

    [add,fix]PocketPrefabの追加。Pocketスクリプトの修正。

--- Assets/Pocket.cs
+++ Assets/Pocket.cs
@@ -5,9 +5,25 @@ using UnityEngine;
 public class Pocket : MonoBehaviour
 {
     [SerializeField] int _clickedAddScore = 1;
+    Material _material;
+    private void Start()
+    {
+        _material = GetComponent<MeshRenderer>().material;
+    }
 
     public void Clicked()
     {
         ScoreManager.Instance.AddScore((int)(_clickedAddScore * GameManager.Instance.ClickMulti));
     }
+
+    public void OnMouseDown()
+    {
+        _material.SetColor("_EmissionColor", Color.white * 0f);
+    }
+
+    public void OnMouseUp()
+    {
+        _material.SetColor("_EmissionColor", Color.white);
+    }
+
 }
