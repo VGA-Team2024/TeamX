7c396dd6249dfbd5a8169a9fa78eb160f634f1f0
Liuisei <116147262+Liuisei@users.noreply.github.com>
2024-02-14 20:12:10 +0900

    [add] FildGanareter , SDatamanager

---
+++ Assets/ScondTeamX/Script/Manager/FildGanareter.cs
@@ -0,0 +1,24 @@
+
+using UnityEngine;
+
+public class FildGanareter : MonoBehaviour
+{
+    [SerializeField] private float _fildToFildRenge = 10;
+    [SerializeField] private GameObject _gameObjectPrefubFild;
+    [SerializeField] private int _fildRangex;
+    [SerializeField] private int _fildRangez;
+
+
+    void Start()
+    {
+        for (int x = 1; x <= _fildRangex; x++)
+        {
+            for (int z = 1; z <= _fildRangez; z++)
+            {
+                Instantiate(_gameObjectPrefubFild,new Vector3(x*_fildToFildRenge, 0 , z*_fildToFildRenge),Quaternion.identity);
+            }
+        }
+        
+    }
+
+}
