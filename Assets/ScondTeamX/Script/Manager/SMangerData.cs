b995e42bf00198a83ac427264beb761e7569c636
a0966891512@gmail.com <a0966891512@gmail.com>
2024-02-16 02:09:59 +0900

    [update] All data in dataManager

--- Assets/ScondTeamX/Script/Manager/SMangerData.cs
+++ Assets/ScondTeamX/Script/Manager/SMangerData.cs
@@ -2,39 +2,136 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SMangerData : Singleton<SMangerData>
 {
-    [SerializeField] private List<BildingStruct> _bildingStructs = new List<BildingStruct>();
+    [Header("Game")] [SerializeField] private List<BuldingStruct> _buildingStructs = new List<BuldingStruct>();
+    [SerializeField] private int _lastx, _lastz;
+    [SerializeField] private EnumGameMode _eGameMode = EnumGameMode.Normal;
+
+
+    [Header("Field")] [SerializeField] private float _fieldBildHight;
+    [SerializeField] private float _fieldToFieldRenge = 10;
+    [SerializeField] private GameObject _fieldgameObjectPrefubFild;
+
+    [Header("Selector")] [SerializeField] private GameObject _OBJSelectorView;
+
+    public event Action OnOBJSelectorViewChanged;
+
+
+    public GameObject ObjSelectorView
+    {
+        get => _OBJSelectorView;
+        set
+        {
+            _OBJSelectorView = value;
+            OnOBJSelectorViewChanged?.Invoke();
+        }
+    }
+
+
+    public List<BuldingStruct> BuildingStructs
+    {
+        get => _buildingStructs;
+        set => _buildingStructs = value;
+    }
+
+    public int Lastx
+    {
+        get => _lastx;
+        set => _lastx = value;
+    }
+
+    public int Lastz
+    {
+        get => _lastz;
+        set => _lastz = value;
+    }
+
+    public EnumGameMode EGameMode
+    {
+        get => _eGameMode;
+        set => _eGameMode = value;
+    }
+
+
+    public float FieldBildHight
+    {
+        get => _fieldBildHight;
+        set => _fieldBildHight = value;
+    }
+
+    public float FieldToFieldRenge
+    {
+        get => _fieldToFieldRenge;
+        set => _fieldToFieldRenge = value;
+    }
+
+    public GameObject FieldgameObjectPrefubFild
+    {
+        get => _fieldgameObjectPrefubFild;
+        set => _fieldgameObjectPrefubFild = value;
+    }
+
+    public int FieldRangex
+    {
+        get => _fieldRangex;
+        set => _fieldRangex = value;
+    }
+
+    public int FieldRangez
+    {
+        get => _fieldRangez;
+        set => _fieldRangez = value;
+    }
+
+    public Quaternion Fieldquaternion
+    {
+        get => _fieldquaternion;
+        set => _fieldquaternion = value;
+    }
+
+    [SerializeField] private int _fieldRangex, _fieldRangez;
+    [SerializeField] private Quaternion _fieldquaternion;
+
+
     protected override void AwakeFunction()
     {
     }
-
-    public List<BildingStruct> BildingStructs
-    {
-        get { return _bildingStructs; }
-    }
-
 }
+
+
 [Serializable]
-public struct BildingStruct
+public struct BuldingStruct
 {
     [SerializeField] private GameObject _gameObjectBilding;
-    [SerializeField] private int _x, _z;
+    [SerializeField] private Texture _texture;
+    [SerializeField] private string _name;
 
     public GameObject GameObjectBilding
     {
-        set { _gameObjectBilding = value;}
-        get { return _gameObjectBilding; }
+        get => _gameObjectBilding;
+        set => _gameObjectBilding = value;
     }
-    public int X
+
+    public Texture BuldingTexture
     {
-        set { _x = value; }
-        get { return _x; }
+        get => _texture;
+        set => _texture = value;
     }
-    public int Z
+
+    public string BuldingName
     {
-        set { _z = value; }
-        get { return _z; }
+        get => _name;
+        set => _name = value;
     }
+}
+
+public enum EnumGameMode
+{
+    Normal,
+    Bliding,
+    War,
+    Puse
 }
