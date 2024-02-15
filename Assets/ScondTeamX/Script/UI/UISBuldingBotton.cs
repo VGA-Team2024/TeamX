bd59e9f137f71e7224584a238f374652a5fc50b9
a0966891512@gmail.com <a0966891512@gmail.com>
2024-02-16 02:16:33 +0900

    [add script and ui]Bulding Botton , Scroll Bulding Botton, Game ui

---
+++ Assets/ScondTeamX/Script/UI/UISBuldingBotton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class UISBuldingBotton : MonoBehaviour
+{
+    [SerializeField] private RawImage _rawImage;
+    [SerializeField] private TMP_Text _tmpName;
+    [SerializeField] private int _structNum;
+
+    public int StructNum
+    {
+        get => _structNum;
+        set => _structNum = value;
+    }
+
+    private Texture _texture;
+    public Texture BuldingTexture
+    {
+        set { _rawImage.texture = value; }
+    }
+    public string BuldingName
+    {
+        set { _tmpName.SetText(value); }
+    }
+
+    public void OnbottonClick()
+    {
+        SMangerData.Instance.ObjSelectorView = SMangerData.Instance.BuildingStructs[_structNum].GameObjectBilding;
+    }
+}
---
+++ Assets/ScondTeamX/Script/UI/UISGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISGame : MonoBehaviour
+{
+
+
+    void BildingBotton()
+    {
+        SMangerData.Instance.EGameMode = EnumGameMode.Bliding;
+    }
+    
+}
