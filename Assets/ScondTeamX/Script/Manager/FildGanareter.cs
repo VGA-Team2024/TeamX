794037b8e9853f2de481b006dece42f5e02a8f24
a0966891512@gmail.com <a0966891512@gmail.com>
2024-02-15 22:21:39 +0900

    [add] test prefub home [update] Datamanager struct

--- Assets/ScondTeamX/Script/Manager/FildGanareter.cs
+++ Assets/ScondTeamX/Script/Manager/FildGanareter.cs
@@ -1,4 +1,4 @@
-
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FildGanareter : MonoBehaviour
@@ -8,18 +8,48 @@ public class FildGanareter : MonoBehaviour
     [SerializeField] private int _fildRangex;
     [SerializeField] private int _fildRangez;
 
+    [SerializeField] private Quaternion _quaternion;
+    [SerializeField] private float _bildHight;
+    private List<Gurid> _gurids = new List<Gurid>();
+    
+
 
     void Start()
+    {
+        GuridGararete();
+        BildGararete();
+    }
+
+    public void GuridGararete()
     {
         for (int x = 1; x <= _fildRangex; x++)
         {
             for (int z = 1; z <= _fildRangez; z++)
             {
-                var newGurid =Instantiate(_gameObjectPrefubFild,new Vector3(x*_fildToFildRenge, 0 , z*_fildToFildRenge),Quaternion.identity);
-                newGurid.GetComponent<Gurid>().x = x;
-                newGurid.GetComponent<Gurid>().Y = z;
+                var newGurid = Instantiate(_gameObjectPrefubFild, 
+                    new Vector3(x * _fildToFildRenge, 0, z * _fildToFildRenge), _quaternion);
+                var newGuridScript = newGurid.GetComponent<Gurid>();
+                newGuridScript.x = x;
+                newGuridScript.Y = z;
+                _gurids.Add(newGuridScript);
+                newGurid.transform.SetParent(this.transform);
             }
         }
     }
 
+    public void BildGararete()
+    {
+        var blidingdatas = SMangerData.Instance.BildingStructs;
+        foreach (var bildingStruct in blidingdatas)
+        {
+            BildSpown(bildingStruct.GameObjectBilding, bildingStruct.X, bildingStruct.Z);
+        }
+    }
+
+    public void BildSpown(GameObject bildOBJ, int x, int z)
+    {
+        Instantiate(bildOBJ, new Vector3(x * _fildToFildRenge, _bildHight, z * _fildToFildRenge),_quaternion);
+    }
+    
+    
 }
--- Assets/ScondTeamX/Script/Manager/SMangerData.cs
+++ Assets/ScondTeamX/Script/Manager/SMangerData.cs
@@ -1,10 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SMangerData : Singleton<SMangerData>
 {
+    [SerializeField] private List<BildingStruct> _bildingStructs = new List<BildingStruct>();
     protected override void AwakeFunction()
     {
+    }
+
+    public List<BildingStruct> BildingStructs
+    {
+        get { return _bildingStructs; }
+    }
+
+}
+[Serializable]
+public struct BildingStruct
+{
+    [SerializeField] private GameObject _gameObjectBilding;
+    [SerializeField] private int _x, _z;
+
+    public GameObject GameObjectBilding
+    {
+        set { _gameObjectBilding = value;}
+        get { return _gameObjectBilding; }
+    }
+    public int X
+    {
+        set { _x = value; }
+        get { return _x; }
+    }
+    public int Z
+    {
+        set { _z = value; }
+        get { return _z; }
     }
 }
