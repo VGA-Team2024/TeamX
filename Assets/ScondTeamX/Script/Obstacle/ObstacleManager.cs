668904c2792083ecae05fd7df9a02bb823625147
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-02-26 15:24:04 +0900

    [add]見えない壁を追加

---
+++ Assets/ScondTeamX/Script/Obstacle/ObstacleManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    [SerializeField, Tooltip("見えない壁になるObject")] GameObject _obstacleObj;
+
+    private void Start()
+    {
+        _obstacleObj.SetActive(false);
+    }
+    /// <summary>
+    /// 壁を有効にする
+    /// </summary>
+    public void TrueObj()
+    {
+        _obstacleObj.SetActive(true);
+    }
+
+    /// <summary>
+    /// 壁を無効にする
+    /// </summary>
+    public void FalseObj()
+    {
+        _obstacleObj.SetActive(false);
+    }
+}
