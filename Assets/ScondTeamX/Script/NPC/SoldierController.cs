6027096c723e29e3688969beebef29a352d09b62
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2024-02-26 16:02:51 +0900

    [fix]ステートを変更するメソッドを追加

--- Assets/ScondTeamX/Script/NPC/SoldierController.cs
+++ Assets/ScondTeamX/Script/NPC/SoldierController.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SoldierController : NPCBase
+﻿public class SoldierController : NPCBase
 {
     /// <summary>
     /// 現在の兵士の状態
@@ -35,21 +31,30 @@ public class SoldierController : NPCBase
         {
             Agent.isStopped = true;
             IsPatrol = false;
-        }  
-        else if(_workerState == SoldierState.Move)
+        }
+        else if (_workerState == SoldierState.Move)
         {
             IsPatrol = false;
             Agent.isStopped = false;
             Agent.destination = Destination;
         }//移動時の状態
-        else if(_workerState == SoldierState.Patrol)
+        else if (_workerState == SoldierState.Patrol)
         {
-            if(!IsPatrol)
+            if (!IsPatrol)
             {
                 IsPatrol = true;
                 BasePosition = this.transform.position;
             }
             Patrol();
         }//うろうろする処理
+    }
+
+    /// <summary>
+    /// Stateを変える
+    /// </summary>
+    /// <param name="stateName">変えたいステート</param>
+    public void ChangeState(SoldierState stateName)
+    {
+        _workerState = stateName;
     }
 }
--- Assets/ScondTeamX/Script/NPC/WorkerController.cs
+++ Assets/ScondTeamX/Script/NPC/WorkerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static SoldierController;
 
 public class WorkerController : NPCBase
 {
@@ -59,5 +60,14 @@ public class WorkerController : NPCBase
             }
             Patrol();
         }//うろうろする処理
+    }
+
+    /// <summary>
+    /// Stateを変える
+    /// </summary>
+    /// <param name="stateName">変えたいステート</param>
+    public void ChangeState(WorkerState stateName)
+    {
+        _workerState = stateName;
     }
 }
